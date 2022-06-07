bd5b94921c47297bbe443f206a37a3a2cc3da5c9
Tim Fallon <tfallon@ucsd.edu>
2022-06-07 14:20:11 -0700

    update

--- primer3.nf
+++ primer3.nf
@@ -7,11 +7,14 @@ process primer3_index {
     tag "$fasta"
     input:
       path fasta    
-    //output:
-    //
+    output:
+      path "${fasta}*.list"
     shell:
       '''
-          
+glistmaker !{fasta} -w 11          
+glistmaker !{fasta} -w 16          
+ln out_11.list !{fasta}_11.list
+ln out_16.list !{fasta}_16.list
       '''
 }
 
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-nextflow run -resume primer3.nf --genome ~/scratch/Projects/20210918_12B1_v0.3/12B1_scaffolds_v0.3.fasta
+##Emulating a search
+seqkit locate -i --gtf -p "ATGGGAGGAGAAGGGTATCGCGG" ~/scratch/Projects/20210918_12B1_v0.3/12B1_scaffolds_v0.3.fasta > target.gtf
+seqkit subseq --gtf target.gtf -u 500 -d 500 ~/scratch/Projects/20210918_12B1_v0.3/12B1_scaffolds_v0.3.fasta > target.fa
+
+
+nextflow run -resume primer3.nf --genome ~/scratch/Projects/20210918_12B1_v0.3/12B1_scaffolds_v0.3.fasta --target target.fa
